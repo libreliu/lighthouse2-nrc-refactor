3b0e09f2aa3f5a487e2fb59f036af2ad92f55e9d
Libre Liu <libreliu@foxmail.com>
2022-03-26 21:44:09 +0800

    Add nrcapp

---
+++ apps/nrcapp/main.cpp
@@ -0,0 +1,119 @@
+/* main.cpp - Copyright 2019/2021 Utrecht University
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+	   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+#include "platform.h"
+#include "rendersystem.h"
+#include <bitset>
+
+static RenderAPI* renderer = 0;
+static GLTexture* renderTarget = 0;
+static Shader* shader = 0;
+static uint scrwidth = 0, scrheight = 0, car = 0, scrspp = 1;
+static bool running = true;
+static std::bitset<1024> keystates;
+
+#include "main_tools.h"
+
+//  +-----------------------------------------------------------------------------+
+//  |  PrepareScene                                                               |
+//  |  Initialize a scene.                                                  LH2'21|
+//  +-----------------------------------------------------------------------------+
+void PrepareScene()
+{
+	// initialize scene
+	renderer->AddScene( "scene.gltf", "../_shareddata/pica/" );
+	renderer->SetNodeTransform( renderer->FindNode( "RootNode (gltf orientation matrix)" ), mat4::RotateX( -PI / 2 ) );
+	int lightMat = renderer->AddMaterial( make_float3( 100, 100, 80 ) );
+	int lightQuad = renderer->AddQuad( make_float3( 0, -1, 0 ), make_float3( 0, 26.0f, 0 ), 6.9f, 6.9f, lightMat );
+	renderer->AddInstance( lightQuad );
+	car = renderer->AddInstance( renderer->AddMesh( "legocar.obj", "../_shareddata/", 10.0f ) );
+}
+
+//  +-----------------------------------------------------------------------------+
+//  |  HandleInput                                                                |
+//  |  Process user input.                                                  LH2'21|
+//  +-----------------------------------------------------------------------------+
+void HandleInput( float frameTime )
+{
+	// handle keyboard input
+	float spd = (keystates[GLFW_KEY_LEFT_SHIFT] ? 15.0f : 5.0f) * frameTime, rot = 2.5f * frameTime;
+	Camera* camera = renderer->GetCamera();
+	if (keystates[GLFW_KEY_A]) camera->TranslateRelative( make_float3( -spd, 0, 0 ) );
+	if (keystates[GLFW_KEY_D]) camera->TranslateRelative( make_float3( spd, 0, 0 ) );
+	if (keystates[GLFW_KEY_W]) camera->TranslateRelative( make_float3( 0, 0, spd ) );
+	if (keystates[GLFW_KEY_S]) camera->TranslateRelative( make_float3( 0, 0, -spd ) );
+	if (keystates[GLFW_KEY_R]) camera->TranslateRelative( make_float3( 0, spd, 0 ) );
+	if (keystates[GLFW_KEY_F]) camera->TranslateRelative( make_float3( 0, -spd, 0 ) );
+	if (keystates[GLFW_KEY_UP]) camera->TranslateTarget( make_float3( 0, -rot, 0 ) );
+	if (keystates[GLFW_KEY_DOWN]) camera->TranslateTarget( make_float3( 0, rot, 0 ) );
+	if (keystates[GLFW_KEY_LEFT]) camera->TranslateTarget( make_float3( -rot, 0, 0 ) );
+	if (keystates[GLFW_KEY_RIGHT]) camera->TranslateTarget( make_float3( rot, 0, 0 ) );
+}
+
+//  +-----------------------------------------------------------------------------+
+//  |  main                                                                       |
+//  |  Application entry point.                                             LH2'21|
+//  +-----------------------------------------------------------------------------+
+int main()
+{
+	// Initialize staticically linked FreeImage
+	FreeImage_Initialise(FALSE);
+
+	// initialize OpenGL
+	InitGLFW();
+	// initialize renderer
+	renderer = RenderAPI::CreateRenderAPI( "RenderCore_Optix7" );	// OPTIX7 core, best for RTX devices
+	renderer->DeserializeCamera( "camera.xml" );
+	// initialize scene
+	PrepareScene();
+	// set initial window size
+	ReshapeWindowCallback( 0, SCRWIDTH, SCRHEIGHT );
+	// enter main loop
+	while (!glfwWindowShouldClose( window ))
+	{
+		// update scene
+		renderer->SynchronizeSceneData();
+		// render
+		renderer->Render( Restart /* alternative: converge */ );
+		// handle user input
+		HandleInput( 0.025f );
+		// minimal rigid animation example
+		static float r = 0;
+		mat4 M = mat4::RotateY( r * 2.0f ) * mat4::RotateZ( 0.2f * sinf( r * 8.0f ) ) * mat4::Translate( make_float3( 0, 5, 0 ) );
+		renderer->SetNodeTransform( car, M );
+		if ((r += 0.025f * 0.3f) > 2 * PI) r -= 2 * PI;
+		// finalize and present
+		shader->Bind();
+		shader->SetInputTexture( 0, "color", renderTarget );
+		shader->SetInputMatrix( "view", mat4::Identity() );
+		DrawQuad();
+		shader->Unbind();
+		// finalize
+		glfwSwapBuffers( window );
+		glfwPollEvents();
+		if (!running) break; // esc was pressed
+	}
+	// clean up
+	renderer->SerializeCamera( "camera.xml" );
+	renderer->Shutdown();
+	glfwDestroyWindow( window );
+	glfwTerminate();
+
+	// Deinitialize staticically linked FreeImage
+	FreeImage_DeInitialise();
+	return 0;
+}
+
+// EOF
---
+++ apps/nrcapp/main_tools.h
@@ -0,0 +1,114 @@
+/* main_tools.h - Copyright 2019/2021 Utrecht University
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+	   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+GLFWwindow* window = 0;
+
+//  +-----------------------------------------------------------------------------+
+//  |  ...Callback                                                                |
+//  |  Various GLFW callbacks, mostly just forwarded to AntTweakBar.        LH2'19|
+//  +-----------------------------------------------------------------------------+
+void ReshapeWindowCallback( GLFWwindow* window, int w, int h )
+{
+	// don't resize if nothing changed or the window was minimized
+	if ((scrwidth == w && scrheight == h) || w == 0 || h == 0) return;
+	scrwidth = w, scrheight = h;
+	delete renderTarget;
+	renderTarget = new GLTexture( scrwidth, scrheight, GLTexture::FLOAT );
+	glViewport( 0, 0, scrwidth, scrheight );
+	renderer->SetTarget( renderTarget, 1 );
+}
+void KeyEventCallback( GLFWwindow* window, int key, int scancode, int action, int mods )
+{
+	if (key == GLFW_KEY_ESCAPE) running = false;
+	if (action == GLFW_PRESS) keystates[key] = true;
+	else if (action == GLFW_RELEASE) keystates[key] = false;
+}
+void CharEventCallback( GLFWwindow* window, uint code ) { /* nothing here yet */ }
+void MouseButtonCallback( GLFWwindow* window, int button, int action, int mods ) { /* nothing here yet */ }
+void MousePosCallback( GLFWwindow* window, double x, double y )
+{
+	// set pixel probe pos for triangle picking
+	if (renderer) renderer->SetProbePos( make_int2( (int)x, (int)y ) );
+}
+void ErrorCallback( int error, const char*description )
+{
+	fprintf( stderr, "GLFW Error: %s\n", description );
+}
+
+//  +-----------------------------------------------------------------------------+
+//  |  InitGLFW                                                                   |
+//  |  Opens a GL window using GLFW.                                        LH2'19|
+//  +-----------------------------------------------------------------------------+
+void OpenConsole();
+void InitGLFW()
+{
+	// open a window
+	if (!glfwInit()) exit( EXIT_FAILURE );
+	glfwSetErrorCallback( ErrorCallback );
+	glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 4 );
+	glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 5 );
+	glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
+	glfwWindowHint( GLFW_RESIZABLE, GL_TRUE );
+	if (!(window = glfwCreateWindow( SCRWIDTH, SCRHEIGHT, "LightHouse v2.0", nullptr, nullptr ))) exit( EXIT_FAILURE );
+	glfwMakeContextCurrent( window );
+	// register callbacks
+	glfwSetFramebufferSizeCallback( window, ReshapeWindowCallback );
+	glfwSetKeyCallback( window, KeyEventCallback );
+	glfwSetMouseButtonCallback( window, MouseButtonCallback );
+	glfwSetCursorPosCallback( window, MousePosCallback );
+	glfwSetCharCallback( window, CharEventCallback );
+	// initialize GLAD
+	if (!gladLoadGLLoader( (GLADloadproc)glfwGetProcAddress )) exit( EXIT_FAILURE );
+	// prepare OpenGL state
+	glDisable( GL_DEPTH_TEST );
+	glDisable( GL_CULL_FACE );
+	glDisable( GL_BLEND );
+	// logo
+	GLTexture* logo = new GLTexture( "data/system/logo.png", GL_LINEAR );
+	shader = new Shader( "shaders/vignette.vert", "shaders/vignette.frag" );
+	shader->Bind();
+	shader->SetInputTexture( 0, "color", logo );
+	float hscale = ((float)SCRHEIGHT / SCRWIDTH) * ((float)logo->width / logo->height);
+	shader->SetInputMatrix( "view", mat4::Scale( make_float3( 0.1f * hscale, 0.1f, 1 ) ) );
+	DrawQuad();
+	shader->Unbind();
+	glfwSwapBuffers( window );
+	delete logo;
+	// we want a console window for text output
+	OpenConsole();
+}
+
+//  +-----------------------------------------------------------------------------+
+//  |  OpenConsole                                                                |
+//  |  Create the console window for text output.                           LH2'19|
+//  +-----------------------------------------------------------------------------+
+void OpenConsole()
+{
+#ifdef _MSC_VER
+	CONSOLE_SCREEN_BUFFER_INFO coninfo;
+	AllocConsole();
+	GetConsoleScreenBufferInfo( GetStdHandle( STD_OUTPUT_HANDLE ), &coninfo );
+	coninfo.dwSize.X = 1280;
+	coninfo.dwSize.Y = 800;
+	SetConsoleScreenBufferSize( GetStdHandle( STD_OUTPUT_HANDLE ), coninfo.dwSize );
+	FILE* file = nullptr;
+	freopen_s( &file, "CON", "w", stdout );
+	freopen_s( &file, "CON", "w", stderr );
+	SetWindowPos( GetConsoleWindow(), HWND_TOP, 0, 0, 1280, 800, 0 );
+	glfwShowWindow( window );
+#endif
+}
+
+// EOF
